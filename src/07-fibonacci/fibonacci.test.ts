d16fc56ce5a2a03d03761b48281790368096481f
Emilio Calejero <emilio.calejero.es>
2025-09-20 20:58:10 +0200

    Kata fibonaccia: First item of serie

--- src/07-fibonacci/fibonacci.test.ts
+++ src/07-fibonacci/fibonacci.test.ts
@@ -6,21 +6,31 @@
  *   Segundo: 1
  *   Siguientes: la suma de los dos anteriores
  * 
+ *   f(0) = 1, f(1) = 1, f(n) = f(n-1) + f(n-2)
+ * 
  * Serie de Fibonacci: 
  * 
  *   | --------------------------------------------------------------------------------------- |
- *   | 1 | 2 | 3 | 4 | 5 | 6 | 7 |  8 |  9 | 10 | 11 | 12 |  13 |  14 |  15 |  16 |  17 |   18 |
+ *   | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7  |  8 |  9 | 10 | 11 | 12  |  13 |  14 |  15 |  16 |  17  |
  *   | --------------------------------------------------------------------------------------- |
  *   | 0 | 1 | 1 | 2 | 3 | 5 | 8 | 13 | 21 | 34 | 55 | 89 | 144 | 233 | 377 | 610 | 987 | 1597 |
  *   | --------------------------------------------------------------------------------------- | 
  * 
  * Pruebas
- * - Para n igual a 1 devuelve 0 (primero número de la serie)
- * - Para n igual a 2 devuelve 1 (segundo número)
- * - Para n igual a 3 devuelve 1 (la suma de los dos anteriores)
- * - Para n igual a 4 devuelve 2
- * - Para n igual a 10 devuelve 34
- * - Para n igual a 18 devuelve 1597
+ * - Para n igual a 0 devuelve 0 (primero número de la serie)
+ * - Para n igual a 1 devuelve 1 (segundo número)
+ * - Para n igual a 2 devuelve 1 (la suma de los dos anteriores)
+ * - Para n igual a 3 devuelve 2
+ * - Para n igual a 10 devuelve 55
+ * - Para n igual a 17 devuelve 1597
  * - Para n <= 0 devuelve un error
  * 
  */
+
+import { fibonnaci } from "./fibonacci";
+
+describe('Fibonacci', () => {
+    it('The first element of the serie is 0', () => {
+        expect(fibonnaci(0)).toBe(0);
+    });
+});
---
+++ src/07-fibonacci/fibonacci.ts
@@ -0,0 +1,3 @@
+export function fibonnaci(n: number): number {
+    return 0;
+}
