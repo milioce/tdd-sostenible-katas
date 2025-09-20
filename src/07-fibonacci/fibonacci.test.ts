560dcddbfe6372e2b2aa0ad564def7bf04cb7a49
Emilio Calejero <emilio.calejero.es>
2025-09-20 20:51:14 +0200

    Kata fibonacci: Definition of Fibonacci Kata

---
+++ src/07-fibonacci/fibonacci.test.ts
@@ -0,0 +1,26 @@
+/**
+ * Kata Fibonacci: Construir una función que reciba un número entero n y devuelva el n-ésimo número de la serie de Fibonacci
+ * 
+ * Reglas de la serie de Fibonacci: 
+ *   Primero: 0
+ *   Segundo: 1
+ *   Siguientes: la suma de los dos anteriores
+ * 
+ * Serie de Fibonacci: 
+ * 
+ *   | --------------------------------------------------------------------------------------- |
+ *   | 1 | 2 | 3 | 4 | 5 | 6 | 7 |  8 |  9 | 10 | 11 | 12 |  13 |  14 |  15 |  16 |  17 |   18 |
+ *   | --------------------------------------------------------------------------------------- |
+ *   | 0 | 1 | 1 | 2 | 3 | 5 | 8 | 13 | 21 | 34 | 55 | 89 | 144 | 233 | 377 | 610 | 987 | 1597 |
+ *   | --------------------------------------------------------------------------------------- | 
+ * 
+ * Pruebas
+ * - Para n igual a 1 devuelve 0 (primero número de la serie)
+ * - Para n igual a 2 devuelve 1 (segundo número)
+ * - Para n igual a 3 devuelve 1 (la suma de los dos anteriores)
+ * - Para n igual a 4 devuelve 2
+ * - Para n igual a 10 devuelve 34
+ * - Para n igual a 18 devuelve 1597
+ * - Para n <= 0 devuelve un error
+ * 
+ */
