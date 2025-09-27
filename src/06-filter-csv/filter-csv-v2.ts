e196373a5d2a1110654e122045bb1f9652dd57ce
Emilio Calejero <emilio.calejero.es>
2025-09-27 12:00:04 +0200

    Kata FilterCSV version 2: Remove lines with IVA and IGIC empty

--- src/06-filter-csv/filter-csv-v2.ts
+++ src/06-filter-csv/filter-csv-v2.ts
@@ -1,11 +1,27 @@
 export class FilterCSV {
-    constructor(private lines: string[]) {}
+  constructor(private lines: string[]) {}
 
-    public filter(): string[] {
-        const header = this.lines[0];
-        const invoices = this.lines.slice(1).map(line => line.split(','));
-        const filteredInvoices = invoices.filter(line => !(String(line[4]) !== '' && String(line[5]) !== ''));
-        const lines = filteredInvoices.map(line => line.join(','));
-        return [header, ...lines];
+  public filter(): string[] {
+    const header = this.lines[0];
+    const invoices = this.lines.slice(1).map((line) => line.split(','));
+    let filteredInvoices = invoices.filter(this.validIvaAndIgic);
+    const lines = filteredInvoices.map(this.convertToCSV);
+    return [header, ...lines];
+  }
+
+  private validIvaAndIgic(invoiceLine: string[]): boolean {
+    if (String(invoiceLine[4]) !== '' && String(invoiceLine[5]) !== '') {
+      return false;
     }
+
+    if (invoiceLine[4] === '' && invoiceLine[5] === '') {
+      return false;
+    }
+
+    return true;
+  }
+
+  private convertToCSV(invoiceLine: string[]): string {
+    return invoiceLine.join(',');
+  }
 }
