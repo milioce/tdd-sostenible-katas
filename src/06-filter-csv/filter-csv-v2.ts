3d6692285e2157bfcdd77b6940ffaee1a9b7951e
Emilio Calejero <emilio.calejero.es>
2025-09-28 12:26:38 +0200

    Kata FilterCSV version 2: Remove lines with CIF and NIF filled

--- src/06-filter-csv/filter-csv-v2.ts
+++ src/06-filter-csv/filter-csv-v2.ts
@@ -4,9 +4,14 @@ export class FilterCSV {
   public filter(): string[] {
     const header = this.lines[0];
     const invoices = this.lines.slice(1).map((line) => line.split(','));
-    let filteredInvoices = invoices.filter(this.validIvaAndIgic);
+    const filteredInvoices = invoices.filter(this.validInvoice.bind(this));
+
     const lines = filteredInvoices.map(this.convertToCSV);
     return [header, ...lines];
+  }
+
+  private validInvoice(invoiceLine: string[]): boolean {
+    return this.validIvaAndIgic(invoiceLine) && this.validCifAndNif(invoiceLine);
   }
 
   private validIvaAndIgic(invoiceLine: string[]): boolean {
@@ -16,6 +21,14 @@ export class FilterCSV {
 
     if (invoiceLine[4] === '' && invoiceLine[5] === '') {
       return false;
+    }
+
+    return true;
+  }
+
+  private validCifAndNif(invoiceLine: string[]) : boolean {
+    if (invoiceLine[7] !== '' && invoiceLine[8] !== '') {
+        return false;
     }
 
     return true;
