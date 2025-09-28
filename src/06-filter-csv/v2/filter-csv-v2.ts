70efff8f501b0c2176c31ded156986937a2337b0
Emilio Calejero <emilio.calejero.es>
2025-09-28 13:03:25 +0200

    Kata FilterCSV version 2: Refactor - Create class InvoiceLine

---
+++ src/06-filter-csv/v2/filter-csv-v2.ts
@@ -0,0 +1,39 @@
+import { InvoiceLine } from "./invoice-line";
+
+export class FilterCSV {
+  constructor(private lines: string[]) {}
+
+  public filter(): string[] {
+    const header = this.lines[0];
+    const invoices = this.lines.slice(1).map((line) => InvoiceLine.fromCSV(line));
+    const filteredInvoices = invoices.filter(this.validInvoiceLine.bind(this));
+    const lines = filteredInvoices.map(invoice => invoice.toCSV());
+    return [header, ...lines];
+  }
+
+  private validInvoiceLine(invoiceLine: InvoiceLine): boolean {
+    return this.validIvaAndIgic(invoiceLine) && this.validCifAndNif(invoiceLine);
+  }
+
+  private validIvaAndIgic(invoiceLine: InvoiceLine): boolean {
+    if (invoiceLine.iva !== '' && invoiceLine.igic !== '') {
+      return false;
+    }
+
+    if (invoiceLine.iva === '' && invoiceLine.igic === '') {
+      return false;
+    }
+
+    return true;
+  }
+
+  private validCifAndNif(invoiceLine: InvoiceLine): boolean {
+    if (invoiceLine.cif !== '' && invoiceLine.nif !== '') {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+
